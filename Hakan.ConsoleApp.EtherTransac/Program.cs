81e90c0c97ebdce76686bd57a63dd01e65169778
Hakan Yildiz <107430060+Hakkology@users.noreply.github.com>
2022-09-05 23:42:55 +0300

    Add files via upload

---
+++ Hakan.ConsoleApp.EtherTransac/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nethereum.Web3;
+using Nethereum.Web3.Accounts;
+using Nethereum.Web3.Accounts.Managed;
+using Nethereum.Hex.HexTypes;
+
+namespace Hakan.ConsoleApp.EtherTransac
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How much would you like to send? ");
+            decimal Amounttobesent = Convert.ToDecimal(Console.ReadLine());
+            var privateKey = ""; //enter your own private key
+
+            MakeTransaction(privateKey, Amounttobesent).Wait();
+            Console.WriteLine("Transaction Complete.\n" +
+                "------------------------------------");
+
+            GetAccountBalance().Wait();
+            Console.ReadLine();
+        }
+
+        static async Task MakeTransaction(string pkey, decimal amount)
+        {
+            var Accountname = new Account(pkey, Nethereum.Signer.Chain.Kovan);
+            var web3 = new Web3(Accountname, "https://kovan.infura.io/v3/caffea16b05a4541b560f288be086598");
+            var toAddress = "0x6dDA31ACF16d6b214D61BcBe2d57EfBa3042ddB9"; //public key for a friend for testing purposes
+            var transaction = await web3.Eth.GetEtherTransferService().TransferEtherAndWaitForReceiptAsync(toAddress, amount, 3m);
+            System.Diagnostics.Process.Start("https://kovan.etherscan.io/tx/" + transaction.TransactionHash);
+        }
+        static async Task GetAccountBalance()
+        {
+            var web3 = new Web3("https://kovan.infura.io/v3/caffea16b05a4541b560f288be086598");
+            var balance = await web3.Eth.GetBalance.SendRequestAsync("0xd4741Bf17F6857525Cb46f0fb8F78Af20c41263f"); //public key for self
+            Console.WriteLine($"Balance in Wei: {balance.Value}");
+
+            var etherAmount = Web3.Convert.FromWei(balance.Value);
+            Console.WriteLine($"Balance in Ether: {etherAmount}");
+        }
+    }
+}
---
+++ Hakan.ConsoleApp.GetBalance/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Build.Utilities;
+using Nethereum.Web3;
+
+namespace Hakan.ConsoleApp.GetBalance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GetAccountBalance().Wait();
+            Console.ReadLine();
+        }
+
+        static async System.Threading.Tasks.Task GetAccountBalance()
+        {
+            var web3 = new Web3("https://kovan.infura.io/v3/caffea16b05a4541b560f288be086598");
+            var balance = await web3.Eth.GetBalance.SendRequestAsync("0x2F2539fd37F0AB30287Bf8d793eD5EbcF53ef7F2");
+            Console.WriteLine($"Balance in Wei: {balance.Value}");
+            
+            var etherAmount = Web3.Convert.FromWei(balance.Value);
+            Console.WriteLine($"Balance in Ether: {etherAmount}");
+        }
+    }
+}
+
